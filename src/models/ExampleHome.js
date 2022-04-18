387699e5a8eee41c83bcc008be8436d5dfd61f95
Matthew Carter <m@ahungry.com>
2022-04-18 14:08:08 -0400

    Add more code comments

--- src/models/ExampleHome.js
+++ src/models/ExampleHome.js
@@ -2,6 +2,7 @@ const Selector = require('testcafe').Selector
 const { engine } = require('../step_definitions/hooks')
 
 class ExampleHome {
+  // The various page selectors, mapped as CSS selectors, or XPath (both valid)
   selectors = {
     mainHeader: 'div > h1',
   }
@@ -10,12 +11,18 @@ class ExampleHome {
     this.engine = engine
   }
 
+  // In general, 'return await' is an anti-pattern, but this is to show how it can be async/await.
   async visit () {
-    return this.engine.navigateTo('http://example.com')
+    return await this.engine.navigateTo('http://example.com')
   }
 
   async getHeader () {
-    return Selector(this.selectors.mainHeader).with({ boundTestRun: this.engine }).innerText
+    // The bound test segment on the Selector is a requirement for using it with testcafe like this.
+    // it makes it look more complex than it actually is.
+    const element = await Selector(this.selectors.mainHeader).with({ boundTestRun: this.engine })
+
+    // Just like an HTML DOM Element, we can access the text property.
+    return element.innerText
   }
 }
 
